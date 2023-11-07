ab0f99e99c77acf682cdc4a19927de2e9c3e6120
J-I-H-O <jeongjiho0731@gmail.com>
2023-11-07 14:19:49 +0900

    feat(CommentController): CommentController 초기 개발

---
+++ backend/src/main/java/com/jbnu/ideahub/comment/service/CommentService.java
@@ -0,0 +1,29 @@
+package com.jbnu.ideahub.comment.service;
+
+import com.jbnu.ideahub.comment.dto.request.CommentCreateRequest;
+import com.jbnu.ideahub.comment.dto.request.CommentUpdateRequest;
+import com.jbnu.ideahub.comment.dto.response.CommentResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+    // TODO
+    public Long save(CommentCreateRequest commentCreateRequest) {
+        return 1L;
+    }
+
+    public List<CommentResponse> findAllByEntryId(Long entryId) {
+
+        return null;
+    }
+
+    public void update(Long commentId, CommentUpdateRequest commentUpdateRequest) {
+
+    }
+
+    public void delete(Long commentId) {
+
+    }
+}
