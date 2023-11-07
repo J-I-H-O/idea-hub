77601aa6ea9d360b0b58d77202bcdfb07ac248a3
J-I-H-O <jeongjiho0731@gmail.com>
2023-11-07 17:42:01 +0900

    feat(StarController): StarController 초기 개발

---
+++ backend/src/main/java/com/jbnu/ideahub/star/service/StarService.java
@@ -0,0 +1,16 @@
+package com.jbnu.ideahub.star.service;
+
+import com.jbnu.ideahub.star.dto.request.StarRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Service
+public class StarService {
+    public void save(StarRequest starRequest) {
+
+    }
+
+    public void delete(StarRequest starRequest) {
+
+    }
+}
