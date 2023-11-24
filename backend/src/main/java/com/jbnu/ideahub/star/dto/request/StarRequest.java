2a197a971a6f5f9ca70c3a6833f4d0643397c39f
J-I-H-O <jeongjiho0731@gmail.com>
2023-11-24 18:32:43 +0900

    docs: 추천 등록/삭제 API 문서 작성

--- backend/src/main/java/com/jbnu/ideahub/star/dto/request/StarRequest.java
+++ backend/src/main/java/com/jbnu/ideahub/star/dto/request/StarRequest.java
@@ -1,6 +1,7 @@
 package com.jbnu.ideahub.star.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +11,12 @@ import lombok.RequiredArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StarRequest {
+    private static final String MEMBER_ID_NULL_ERROR_MESSAGE = "멤버 식별자는 NULL일 수 없습니다.";
+    private static final String ENTRY_ID_NULL_ERROR_MESSAGE = "작품 식별자는 NULL일 수 없습니다.";
 
-    private static final String MEMBER_ID_BLANK_ERROR_MESSAGE = "멤버 식별자는 공백일 수 없습니다.";
-    private static final String ENTRY_ID_BLANK_ERROR_MESSAGE = "작품 식별자는 공백일 수 없습니다.";
-
-    @NotBlank(message = MEMBER_ID_BLANK_ERROR_MESSAGE)
+    @NotNull(message = MEMBER_ID_NULL_ERROR_MESSAGE)
     private Long memberId;
 
-    @NotBlank(message = ENTRY_ID_BLANK_ERROR_MESSAGE)
+    @NotNull(message = ENTRY_ID_NULL_ERROR_MESSAGE)
     private Long entryId;
 }
---
+++ backend/src/test/java/com/jbnu/ideahub/restDocs/star/StarControllerRestdocsTest.java
@@ -0,0 +1,77 @@
+package com.jbnu.ideahub.restDocs.star;
+
+import com.jbnu.ideahub.restDocs.common.RestdocsTestController;
+import com.jbnu.ideahub.star.dto.request.StarRequest;
+import com.jbnu.ideahub.star.presentation.StarController;
+import com.jbnu.ideahub.star.service.StarService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static com.jbnu.ideahub.restDocs.utils.ApiDocumentUtils.getDocumentRequest;
+import static com.jbnu.ideahub.restDocs.utils.ApiDocumentUtils.getDocumentResponse;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(StarController.class)
+public class StarControllerRestdocsTest extends RestdocsTestController {
+
+    @MockBean
+    private StarService starService;
+
+    @Test
+    @DisplayName("추천 등록 API")
+    void createStar() throws Exception {
+        // given
+        StarRequest request = new StarRequest(1L, 1L);
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        // when
+        ResultActions result = mockMvc.perform(post("/stars")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest));
+
+        // then
+        result.andExpect(status().isNoContent())
+                .andDo(document("stars/create-star",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("추천을 누른 회원의 id"),
+                                fieldWithPath("entryId").type(JsonFieldType.NUMBER).description("추천을 받은 작품의 id")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("추천 삭제 API")
+    void deleteStar() throws Exception {
+        // given
+        StarRequest request = new StarRequest(1L, 1L);
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        // when
+        ResultActions result = mockMvc.perform(delete("/stars")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest));
+
+        // then
+        result.andExpect(status().isNoContent())
+                .andDo(document("stars/delete-star",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("추천을 누른 회원의 id"),
+                                fieldWithPath("entryId").type(JsonFieldType.NUMBER).description("추천을 받은 작품의 id")
+                        )
+                ));
+    }
+}
