1461ff32c746d2621f64278b9dbc138f34ac4688
J-I-H-O <jeongjiho0731@gmail.com>
2023-12-24 21:38:10 +0900

    docs(EntryControllerRestdocsTest): API 문서에 paging 정보 반영되도록 수정

--- backend/src/main/java/com/jbnu/ideahub/entry/domain/repository/EntryRepository.java
+++ backend/src/main/java/com/jbnu/ideahub/entry/domain/repository/EntryRepository.java
@@ -2,7 +2,6 @@ package com.jbnu.ideahub.entry.domain.repository;
 
 import com.jbnu.ideahub.entry.domain.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface EntryRepository extends JpaRepository<Entry, Long> {
 }
--- backend/src/test/java/com/jbnu/ideahub/document/entry/EntryControllerRestdocsTest.java
+++ backend/src/test/java/com/jbnu/ideahub/document/entry/EntryControllerRestdocsTest.java
@@ -14,6 +14,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -118,8 +122,9 @@ public class EntryControllerRestdocsTest extends RestdocsTestController {
                 .datetimeMetadata(datetimeMetadata)
                 .build();
 
-        given(entryService.findAll())
-                .willReturn(List.of(response1, response2));
+        Page<EntryResponse> findResult = new PageImpl<>(List.of(response1, response2));
+        given(entryService.findAll(any(Pageable.class)))
+                .willReturn(findResult);
 
         // when
         ResultActions result = mockMvc.perform(get("/entries"));
@@ -130,18 +135,31 @@ public class EntryControllerRestdocsTest extends RestdocsTestController {
                         getDocumentRequest(),
                         getDocumentResponse(),
                         responseFields(
-                                fieldWithPath("data[].id").type(JsonFieldType.NUMBER).description("작품 id"),
-                                fieldWithPath("data[].competitionId").type(JsonFieldType.NUMBER).optional().description("해당 작품이 등록된 대회의 id. 대회에 등록한 것이 아닌 작품만 별개로 등록한 경우 null"),
-                                fieldWithPath("data[].title").type(JsonFieldType.STRING).description("작품 제목"),
-                                fieldWithPath("data[].content").type(JsonFieldType.STRING).description("작품 상세 정보"),
-                                fieldWithPath("data[].status").type(JsonFieldType.STRING).attributes(getEnumFormat()).description("작품 진행 상태"),
-                                fieldWithPath("data[].github").type(JsonFieldType.STRING).description("작품 깃허브 주소"),
-                                fieldWithPath("data[].prize").optional().description("작품 수상 정보. 수상 작품이 없다면 null"),
-                                fieldWithPath("data[].prize.prizeName").type(JsonFieldType.STRING).optional().description("수상한 상의 이름"),
-                                fieldWithPath("data[].prize.prizePriority").type(JsonFieldType.NUMBER).optional().description("수상한 상의 우선순위. 상격이 높을수록 낮은 숫자를 가짐"),
-                                fieldWithPath("data[].datetimeMetadata").description("등록 및 수정 시각 정보"),
-                                fieldWithPath("data[].datetimeMetadata.createdAt").type(JsonFieldType.STRING).attributes(getDatetimeFormat()).description("작품 최초 등록 시각"),
-                                fieldWithPath("data[].datetimeMetadata.updatedAt").type(JsonFieldType.STRING).attributes(getDatetimeFormat()).description("작품 최종 수정 시각")
+                                fieldWithPath("data.content[].id").type(JsonFieldType.NUMBER).description("작품 id"),
+                                fieldWithPath("data.content[].competitionId").type(JsonFieldType.NUMBER).optional().description("해당 작품이 등록된 대회의 id. 대회에 등록한 것이 아닌 작품만 별개로 등록한 경우 null"),
+                                fieldWithPath("data.content[].title").type(JsonFieldType.STRING).description("작품 제목"),
+                                fieldWithPath("data.content[].content").type(JsonFieldType.STRING).description("작품 상세 정보"),
+                                fieldWithPath("data.content[].status").type(JsonFieldType.STRING).attributes(getEnumFormat()).description("작품 진행 상태"),
+                                fieldWithPath("data.content[].github").type(JsonFieldType.STRING).description("작품 깃허브 주소"),
+                                fieldWithPath("data.content[].prize").optional().description("작품 수상 정보. 수상 작품이 없다면 null"),
+                                fieldWithPath("data.content[].prize.prizeName").type(JsonFieldType.STRING).optional().description("수상한 상의 이름"),
+                                fieldWithPath("data.content[].prize.prizePriority").type(JsonFieldType.NUMBER).optional().description("수상한 상의 우선순위. 상격이 높을수록 낮은 숫자를 가짐"),
+                                fieldWithPath("data.content[].datetimeMetadata").description("등록 및 수정 시각 정보"),
+                                fieldWithPath("data.content[].datetimeMetadata.createdAt").type(JsonFieldType.STRING).attributes(getDatetimeFormat()).description("작품 최초 등록 시각"),
+                                fieldWithPath("data.content[].datetimeMetadata.updatedAt").type(JsonFieldType.STRING).attributes(getDatetimeFormat()).description("작품 최종 수정 시각"),
+                                fieldWithPath("data.pageable").description("페이지 정보"),
+                                fieldWithPath("data.last").description("현재 페이지가 마지막 페이지인지 여부"),
+                                fieldWithPath("data.totalElements").description("전체 작품의 개수"),
+                                fieldWithPath("data.totalPages").description("전체 페이지 수"),
+                                fieldWithPath("data.size").description("현재 페이지의 작품 수"),
+                                fieldWithPath("data.number").description("현재 페이지 번호"),
+                                fieldWithPath("data.sort").description("정렬 정보"),
+                                fieldWithPath("data.sort.empty").description("정렬 정보가 비어있는지 여부"),
+                                fieldWithPath("data.sort.sorted").description("정렬된 상태인지 여부"),
+                                fieldWithPath("data.sort.unsorted").description("정렬되지 않은 상태인지 여부"),
+                                fieldWithPath("data.first").description("현재 페이지가 첫번째 페이지인지 여부"),
+                                fieldWithPath("data.numberOfElements").description("현재 페이지에 포함된 작품 수"),
+                                fieldWithPath("data.empty").description("데이터가 비어있는지 여부")
                         )
                 ));
     }
@@ -203,7 +221,7 @@ public class EntryControllerRestdocsTest extends RestdocsTestController {
     @DisplayName("작품 수정 API")
     void updateEntry() throws Exception {
         // given
-        Prize prize = new Prize("대상", 1);
+        PrizeDto prize = PrizeDto.of(new Prize("대상", 1));
         EntryUpdateRequest request = EntryUpdateRequest
                 .builder()
                 .competitionId(1L)
